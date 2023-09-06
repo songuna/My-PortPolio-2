9449d2b22065d5b26cf956d069ecc8a9d2716ed0
201601972-kimdongwook <138658065+songuna@users.noreply.github.com>
2023-09-07 06:07:07 +0900

    .

--- index/index.js
+++ index/index.js
@@ -1,5 +1,13 @@
 const input = document.getElementById("input");
+const sc = document.getElementById("sc");
 
 input.addEventListener("keyup", function (event) {
-    if (event.)
+    if (event.key = enter) {
+        event.preventDefault();
+    }
+    alert("enter");
+});
+
+sc.addEventListener('click', function () {
+    alert('click')
 })
