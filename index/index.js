437bad2a7337963304712ebd2eae021a1a2e9704
201601972-kimdongwook <138658065+songuna@users.noreply.github.com>
2023-09-07 07:16:06 +0900

    .

--- index/index.js
+++ index/index.js
@@ -1,5 +1,10 @@
 const input = document.getElementById("input");
 const sc = document.getElementById("sc");
+const itd = document.getElementById("itd");
+const pf = document.getElementById("pf");
+const sk = document.getElementById("sk");
+
+
 
 input.addEventListener("keyup", function (event) {
     if (event.keyCode === 13) {
