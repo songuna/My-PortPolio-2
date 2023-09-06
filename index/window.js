07abb46303d1583ff69a8d761bca92c450ff7c05
201601972-kimdongwook <138658065+songuna@users.noreply.github.com>
2023-09-07 07:32:35 +0900

    .

--- index/window.js
+++ index/window.js
@@ -1,4 +1,4 @@
 
 function openPop() {
-    const popup = window.open('http://www.naver.com', 'naver', 'width=700px,height=800px,scrollbars=yes');
+    const openPop = window.open('http://www.naver.com', 'naver', 'width=700px,height=800px,scrollbars=yes');
 }
