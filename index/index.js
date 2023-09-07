97c7a503c04edabc59ffb004abb98c54c56e6bd3
201601972-kimdongwook <138658065+songuna@users.noreply.github.com>
2023-09-08 05:16:26 +0900

    .

--- index/index.js
+++ index/index.js
@@ -1,21 +1,23 @@
 const input = document.getElementById("input");
 const sc = document.getElementById("sc");
-const itd = document.getElementById("itd");
-const pf = document.getElementById("pf");
-const sk = document.getElementById("sk");
 
-
+const checkInputandOpenPage = function () {
+    if (input.value === '프로필') {
+        window.open('../profile/profile.html')
+    } else if (input.value === '자기소개') {
+        window.open('../introduce/introduce.html')
+    } else if (input.value === '스킬') {
+        window.open('../skills/skills.html')
+    }
+};
 
 input.addEventListener("keyup", function (event) {
     if (event.key === 'Enter') {
         event.preventDefault();
-
-        if (input.value === '프로필') {
-            window.open('../profile/profile.html');
-        }
+        checkInputandOpenPage();
     }
 });
 
 sc.addEventListener('click', function () {
-    alert('click')
+    checkInputandOpenPage();
 })
