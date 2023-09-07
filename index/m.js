5e3b3215e49e9365758ecacd5af547051abf0d6b
201601972-kimdongwook <138658065+songuna@users.noreply.github.com>
2023-09-08 07:54:00 +0900

    물음표 토글바 완성!!

--- index/m.js
+++ index/m.js
@@ -1,8 +1,23 @@
 const m = document.getElementById("m");
 const box = document.getElementById("box");
+const ins = document.getElementById("ins");
+const gh = document.getElementById("gh");
+const mes = document.getElementById("mes");
 
 jQuery(function () {
     jQuery("#m").on("click", function () {
         jQuery("#box").slideToggle("fast");
     });
 });
+
+ins.addEventListener("click", function () {
+    window.location.href = 'https://instagram.com/v2ly_you?igshid=NGVhN2U2NjQ0Yg=='
+})
+
+gh.addEventListener("click", function () {
+    window.location.href = 'https://github.com/songuna'
+})
+
+mes.addEventListener("click", function () {
+    window.location.href = "./kakao.jpg"
+})
